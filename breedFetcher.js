f8a023f91afe8f34081582c68b89c5c4aa114ad6
Neema <patelneema93@gmail.com>
2021-11-04 18:21:02 -0400

    data get

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,25 +2,31 @@
 const request = require("request");
 //get value from cammandline
 const arg = process.argv.slice(2);
+let breedName = arg[0];
 //https://api.thecatapi.com/v1/breeds/search?q=sib
 console.log(arg);
 //make request
-request({
-  url: `https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`,
+const fetchBreedDescription =function(breedName,callback){ request({
+  url: `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
   method: "GET",
 },
-function(error, response, body) {
+function(error,response,body) {
   // error handling
   if (error) {
     console.log("Error:",error);
-  }
-  //get data from the body
-  const data = JSON.parse(body);
-  //check comand line arg not valid
-  if (data.length < 1) {
-    console.log("The requested breed is not found");
+    return error;
   } else {
-    //print description
-    console.log(data[0].description);
+    //get data from the body
+    const data = JSON.parse(body);
+    //check comand line arg not valid
+    if (data.length < 1) {
+        return null;
+      console.log("The requested breed is not found");
+    } else {
+      //print description
+      return data[0].description;
+    }
   }
 });
+}
+module.exports = { fetchBreedDescription };
